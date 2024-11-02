e4cf8ce169c30403b2407e83c90d8c0b833a4933
boutaina2453 <sr875949@gmail.com>
2024-11-02 14:10:59 +0100

    First Commit

---
+++ custmoer-service/src/main/java/org/sid/custmoerservice/CustmoerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.custmoerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustmoerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustmoerServiceApplication.class, args);
+	}
+
+}
